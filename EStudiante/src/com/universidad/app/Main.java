2268e84729a58f81c8097130b27895ef05e8b81a
SamuelZ7021 <samuelandrey1207@gmail.com>
2025-09-17 13:45:29 -0500

    lol

---
+++ EStudiante/src/com/universidad/app/Main.java
@@ -0,0 +1,7 @@
+package com.universidad.app;
+
+public class Main {
+    public static void main(String[] args){
+        new Menu().mostrarMenu();
+    }
+}
---
+++ EStudiante/src/com/universidad/app/Menu.java
@@ -0,0 +1,7 @@
+package com.universidad.app;
+
+public class Menu {
+    public void mostrarMenu() {
+        System.out.println("Hola");
+    }
+}
